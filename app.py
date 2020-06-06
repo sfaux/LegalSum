34dcdb43ebb369a20352ce646475acca18495069
Sydney Faux <48231621+sfaux@users.noreply.github.com>
2020-06-05 20:41:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def index():
                 no_stop_words_count.items(),
                 key=operator.itemgetter(1),
                 reverse=True
-            )
+            )[:10]
             try:
                 result = Result(
                     url=url,
